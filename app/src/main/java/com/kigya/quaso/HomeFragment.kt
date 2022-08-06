9cea99f0c5b6f7c1aa3f5e5b3597f759491aa401
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-08-06 12:57:23 +0300

    Initial commit

---
+++ app/src/main/java/com/kigya/quaso/HomeFragment.kt
@@ -0,0 +1,2 @@
+package com.kigya.quaso
+
---
+++ app/src/main/java/com/kigya/quaso/QuizFragment.kt
@@ -0,0 +1,9 @@
+package com.kigya.quaso
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
