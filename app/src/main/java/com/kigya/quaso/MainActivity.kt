b1e337391c71cd60d13abe963caf38bfb47c02fc
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-08-07 16:49:31 +0300

    Add basic layouts

--- app/src/main/java/com/kigya/quaso/MainActivity.kt
+++ app/src/main/java/com/kigya/quaso/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kigya.quaso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
